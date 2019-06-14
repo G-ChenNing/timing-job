c5932db034d8662b425de3bf850af6bebef72564
G-ChenNing <wcngoodlight@gmail.com>
2019-06-14 18:45:37 +0800

    first commit

---
+++ src/main/java/com/github/wangchenning/timingjob/TimingJobApplication.java
@@ -0,0 +1,13 @@
+//package com.github.wangchenning.timingjob;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class TimingJobApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(TimingJobApplication.class, args);
+//    }
+//
+//}
